10af238ff61fca23e77024326dcb2cbab8a43184
analistadatosrd-prog <analista.datosrd@gmail.com>
2026-09-04 09:38:32 -0500

    Create tablero_alertamientos.py

---
+++ RD_App/modules/tablero_alertamientos.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Tablero de alertamientos",
+    page_icon="🚨",
+    layout="wide",
+)
+
+st.title("Tablero de alertamientos")
+st.caption(
+    "Centro de seguimiento de alertas operativas, comerciales y logísticas."
+)
+
+st.info(
+    "Este módulo se encuentra en desarrollo."
+)
